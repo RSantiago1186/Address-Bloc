34e5d2185ca988171bd77add8a6793501ab5c211
Ray Santiago <rsantiago1186@gmail.com>
2017-05-28 20:06:00 +0000

    Menu continued

--- Address Bloc/controllers/menu_controller.rb
+++ Address Bloc/controllers/menu_controller.rb
@@ -12,11 +12,10 @@ class MenuController
     # Display the main menu options to the command line
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - View entry number n"
-    puts "3 - Create an entry"
-    puts "4 - Search for an entry"
-    puts "5 - Import entries from a CSV"
-    puts "6 - Exit"
+    puts "2 - Create an entry"
+    puts "3 - Search for an entry"
+    puts "4 - Import entries from a CSV"
+    puts "5 - Exit"
     print "Enter your selection: "
 
     # Retrieve user input from the command line with gets
@@ -31,21 +30,17 @@ class MenuController
       main_menu
     when 2
       system "clear"
-      view_entry_n
+      create_entry
       main_menu
     when 3
       system "clear"
-      create_entry
+      search_entries
       main_menu
     when 4
       system "clear"
-      search_entries
-      main_menu
-    when 5
-      system "clear"
       read_csv
       main_menu
-    when 6
+    when 5
       puts "Good-bye!"
       # Terminate the program using exit(0)
       exit(0)
@@ -71,21 +66,6 @@ class MenuController
     puts "End of entries"
   end
 
-  def view_entry_n
-    system "clear"
-    puts "What entry do you want to see?"
-    print "Entry number: "
-    selection = gets.chomp
-
-    @address_book.entries.each_with_index do |entry, index|
-      if selection.to_i - 1 == index
-        puts entry
-      else
-        puts "That's not a valid entry. Try again."
-      end
-    end
-  end
-
   def create_entry
     # Clear the screen before displaying the create entry prompts
     system "clear"
@@ -106,9 +86,65 @@ class MenuController
   end
 
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+
+    match = @address_book.binary_search.name
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
   def read_csv
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = @address_book.import_from_csv(file_name)
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
+  end
+
+  def delete_entry(entry)
+    @address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def demolish
+    @address_book.entries.clear
+    puts "All entries have been deleted"
+  end
+
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.empty = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry: "
+    puts entry
   end
 
   def entry_submenu(entry)
@@ -116,18 +152,23 @@ class MenuController
     puts "n - next entry"
     puts "d - delete entry"
     puts "e - edit this entry"
+    puts "x - delete all entries"
     puts "m - return to main menu"
 
     # Chomp removes any trailing whitespace from the string gets returns
     selection = gets.chomp
 
     case selection
-      # When the user asks to see next entry, we can do nothing, so control is returned to view_all_entries
+      # When the user asks to see next entry, we can do nothing, so control us returned to view_all_entries
     when "n"
       # We will handle deleting and editing in another checkpoint
     when "d"
+      delete_entry(entry)
     when "e"
-      # We return the user to the main menu
+      edit_entry(entry)
+      entry_submenu(entry)
+    when "x"
+      demolish
     when "m"
       system "clear"
       main_menu
@@ -135,6 +176,33 @@ class MenuController
       system "clear"
       puts "#{selection} is not a valid input"
       entries_submenu(entry)
+    end
+  end
+
+  def search_submenu(entry)
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+    case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
     end
   end
 end
